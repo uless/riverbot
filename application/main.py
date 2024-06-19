6d03cb55925283e18c671606d688520414391144
Voldy1998 <40512980+Voldy1998@users.noreply.github.com>
2024-06-19 20:29:28 +0530

    Adding voice support for tell me more, next steps, detailed actions

--- application/main.py
+++ application/main.py
@@ -3,6 +3,7 @@ import uuid
 import secrets
 import socket
 import httpx
+import logging
 
 
 from typing import Annotated
@@ -71,31 +72,52 @@ class MyEventHandler(TranscriptResultStreamHandler):
     def __init__(self, output_stream, websocket):
         super().__init__(output_stream)
         self.websocket = websocket
-        
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        # Mapping of specific phrases to API endpoints
+        self.api_endpoints = {
+            "tell me more": "http://localhost:8000/chat_detailed_api",
+            "next steps": "http://localhost:8000/chat_actionItems_api",
+            "sources": "http://localhost:8000/chat_sources_api",
+        }
+
     async def handle_transcript_event(self, transcript_event: TranscriptEvent):
         results = transcript_event.transcript.results
         for result in results:
             if not result.is_partial:
                 for alt in result.alternatives:
-                    print(alt.transcript)
-                    # Prepare the form data as expected by the /chat_api endpoint
+                    logging.info(f"Handling full transcript: {alt.transcript}")
+                    api_url = self.determine_api_url(alt.transcript.strip().lower())
                     form_data = {'user_query': alt.transcript}
-                    async with httpx.AsyncClient() as client:
-                        response = await client.post("http://localhost:8000/chat_api", data=form_data)
-                        print(response.text)  # Handle or log the response from your chat API
-                        # Send back the user query and bot response to the UI
-                        await self.websocket.send_json({
-                            'type': 'user',
-                            'transcript': alt.transcript
-                        })
-                        print("Sent user message to client.")
-                        # Assuming the API response contains the bot's message
-                        await self.websocket.send_json({
-                            'type': 'bot',
-                            'response': response.json()['resp'],
-                            'messageID': response.json()['msgID']
-                        })
-                        print("Sent bot response to client.") 
+                    try:
+                        async with httpx.AsyncClient() as client:
+                            response = await client.post(api_url, data=form_data)
+                            response.raise_for_status()  # This will raise an exception for HTTP error responses
+                            await self.send_responses(alt.transcript, response)
+                    except (httpx.HTTPError, httpx.RequestError) as e:
+                        logging.error(f"Failed to post to chat API: {e}")
+                        await self.websocket.send_json({'type': 'error', 'details': str(e)})
+
+    def determine_api_url(self, transcript):
+        # Exact matches for specific requests, considering an optional period
+        for key, url in self.api_endpoints.items():
+            if transcript == key or transcript == key + '.':
+                return url
+        return "http://localhost:8000/chat_api"  # Default API if no exact match is found
+
+    async def send_responses(self, user_transcript, api_response):
+        await self.websocket.send_json({
+            'type': 'user',
+            'transcript': user_transcript
+        })
+        logging.info("Sent user message to client.")
+        api_response_data = api_response.json()
+        await self.websocket.send_json({
+            'type': 'bot',
+            'response': api_response_data['resp'],
+            'messageID': api_response_data['msgID']
+        })
+        logging.info("Sent bot response to client.")
+
 
 # Take environment variables from .env
 load_dotenv(override=True)  
--- application/static/js/utils.js
+++ application/static/js/utils.js
@@ -9,25 +9,25 @@ $(document).ready(function () {
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
   });
-  // Function to display a user message in the chat interface
-  function displayUserMessage(userQuery) {
-    const chatHistory = document.getElementById('chatbot-prompt');
-    const userMessage = document.createElement('div');
-    userMessage.classList.add('card', 'user-message', 'right');
-    userMessage.innerHTML = `
+  // // Function to display a user message in the chat interface
+  // function displayUserMessage(userQuery) {
+  //   const chatHistory = document.getElementById('chatbot-prompt');
+  //   const userMessage = document.createElement('div');
+  //   userMessage.classList.add('card', 'user-message', 'right');
+  //   userMessage.innerHTML = `
 
-        <div class="card-body chatbot-question">
-          <div class="row">             
-            <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 col-10 message-body">
-            ${userQuery}
-            </div>
+  //       <div class="card-body chatbot-question">
+  //         <div class="row">             
+  //           <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 col-10 message-body">
+  //           ${userQuery}
+  //           </div>
             
-          </div>
-        </div>
+  //         </div>
+  //       </div>
 
-      `;
-    chatHistory.appendChild(userMessage);
-  }
+  //     `;
+  //   chatHistory.appendChild(userMessage);
+  // }
 
   // Toggle buttons for language selection
 document.getElementById('english-button').addEventListener('click', function() {
@@ -173,116 +173,116 @@ function showReactions(message) {
     </div>`;
   }
   // Function to display a bot message in the chat interface
-  function displayBotMessage(botResponse, messageID) {
-    const chatHistory = document.getElementById('chatbot-prompt');
-    const botMessage = document.createElement('div');
-    botMessage.classList.add('card', 'left');
-    botMessage.classList.add('card', 'bot-message', 'data-messageid-' + messageID);
-    botMessage.innerHTML = `
-        <div class="card-body welcome-message pb-0" data-messageid=${messageID}>
-        <div class="row">
-          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 col-2 d-flex flex-wrap align-items-top justify-content-center">
-            <img class="waterdrop1" />
-          </div>
-          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 col-10 bot-message-body">
-            <p class="m-0" id="botmessage-${messageID}"></p>
-          </div>
-          </div>
-        </div>
-        <div class="card-footer pt-0 p-8" style="padding:8px; border:0;">
-        <div class="row mb-4">
-          <div class="col-2"></div>
-          <div class="col-10" style="padding-top: 0.5rem;">
+  // function displayBotMessage(botResponse, messageID) {
+  //   const chatHistory = document.getElementById('chatbot-prompt');
+  //   const botMessage = document.createElement('div');
+  //   botMessage.classList.add('card', 'left');
+  //   botMessage.classList.add('card', 'bot-message', 'data-messageid-' + messageID);
+  //   botMessage.innerHTML = `
+  //       <div class="card-body welcome-message pb-0" data-messageid=${messageID}>
+  //       <div class="row">
+  //         <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 col-2 d-flex flex-wrap align-items-top justify-content-center">
+  //           <img class="waterdrop1" />
+  //         </div>
+  //         <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-xl-10 col-10 bot-message-body">
+  //           <p class="m-0" id="botmessage-${messageID}"></p>
+  //         </div>
+  //         </div>
+  //       </div>
+  //       <div class="card-footer pt-0 p-8" style="padding:8px; border:0;">
+  //       <div class="row mb-4">
+  //         <div class="col-2"></div>
+  //         <div class="col-10" style="padding-top: 0.5rem;">
          
-          <a class="reaction" title="I like the response" data-messageid=${messageID} data-reaction="1"><i class="bi bi-hand-thumbs-up fa-0.75x"></i></a> 
-          <a class="reaction" data-toggle="tooltip" data-placement="top" title="Could be better" data-messageid=${messageID} data-reaction="0"><i class="bi bi-hand-thumbs-down fa-0.75x"></i></a>
+  //         <a class="reaction" title="I like the response" data-messageid=${messageID} data-reaction="1"><i class="bi bi-hand-thumbs-up fa-0.75x"></i></a> 
+  //         <a class="reaction" data-toggle="tooltip" data-placement="top" title="Could be better" data-messageid=${messageID} data-reaction="0"><i class="bi bi-hand-thumbs-down fa-0.75x"></i></a>
           
-          <!--  <span class="reaction" data-toggle="tooltip" data-placement="top" title="Could be better" data-bs-toggle="modal" data-messageid=${messageID} data-bs-target="#modal-${messageID}" data-reaction="0"><i class="bi bi-hand-thumbs-down fa-0.75x"></i></span> -->
-          <!-- <button type="button" class = "btn btn-sm followup-buttons fw-bold" id="shortButton">
-            Short
-          </button>  -->
-          <a type="button" class = "btn btn-sm followup-buttons fw-bold" id="detailedButton">
-            Tell me more
-          </a>
-          <a type="button" class = "btn btn-sm followup-buttons fw-bold" id="actionItemsButton">
-            Next steps
-          </a>
-          <a type="button" class = "btn btn-sm followup-buttons fw-bold" id="sourcesButton">
-            Sources
-          </a>
-          <!-- <a type="button" class = "btn btn-sm followup-buttons" id="actionItemsButton">
-            <div>Things you can do</div> -->
-          </a>
-          </div>
-        </div>
-        <div id="feedback-${messageID}" class="row" style="display:none;">
-          <div class="col-2"></div>
-          <div class="col-10">
-            <div class="row" style="border: 1px solid #ccc;border-radius: 4px">
-                  <div class="col-3" style="align-self: center;">
-                      <img class="waterdrop4">
-                  </div>
-                  <div class="col-9">
-                    <div class="card-title m-0"></div>
-                    <div class="card-body" data-messageid="449">          
-                    <p class="fw-bold">What did you not like?</p>
-                    <button type="button" class="btn btn-sm feedback-button fw-bold mb-2" data-comment="Factually incorrect" data-messageid=${messageID} id="btnFactuallyIncorrect">
-                      Factually incorrect
-                    </button>
-                    <button type="button" class="btn btn-sm feedback-button fw-bold mb-2" data-comment="Generic response" data-messageid=${messageID} id="btnGenericResponse">
-                      Generic response
-                    </button>
-                    <button type="button" class="btn btn-sm feedback-button fw-bold mb-2" data-comment="Refused to answer" data-messageid=${messageID} id="btnRefusedToAnswer">
-                      Refused to answer
-                    </button>
-                    <button type="button" class="btn btn-sm feedback-other-button fw-bold mb-2" data-bs-toggle="modal" data-messageid=${messageID} data-bs-target="#modal-${messageID}" data-comment="Other" id="btnOther">
-                      Other
-                    </button>
-                    </div>
-                  </div>
-              </div>
-          </div>
-        </div>
-      </div>
-        <!-- Modal -->
-        <div class="modal fade" id="modal-${messageID}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-              <img class="waterdrop3">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">
-                  &nbsp;
-                  Provide additional feedback
-                </h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
+  //         <!--  <span class="reaction" data-toggle="tooltip" data-placement="top" title="Could be better" data-bs-toggle="modal" data-messageid=${messageID} data-bs-target="#modal-${messageID}" data-reaction="0"><i class="bi bi-hand-thumbs-down fa-0.75x"></i></span> -->
+  //         <!-- <button type="button" class = "btn btn-sm followup-buttons fw-bold" id="shortButton">
+  //           Short
+  //         </button>  -->
+  //         <a type="button" class = "btn btn-sm followup-buttons fw-bold" id="detailedButton">
+  //           Tell me more
+  //         </a>
+  //         <a type="button" class = "btn btn-sm followup-buttons fw-bold" id="actionItemsButton">
+  //           Next steps
+  //         </a>
+  //         <a type="button" class = "btn btn-sm followup-buttons fw-bold" id="sourcesButton">
+  //           Sources
+  //         </a>
+  //         <!-- <a type="button" class = "btn btn-sm followup-buttons" id="actionItemsButton">
+  //           <div>Things you can do</div> -->
+  //         </a>
+  //         </div>
+  //       </div>
+  //       <div id="feedback-${messageID}" class="row" style="display:none;">
+  //         <div class="col-2"></div>
+  //         <div class="col-10">
+  //           <div class="row" style="border: 1px solid #ccc;border-radius: 4px">
+  //                 <div class="col-3" style="align-self: center;">
+  //                     <img class="waterdrop4">
+  //                 </div>
+  //                 <div class="col-9">
+  //                   <div class="card-title m-0"></div>
+  //                   <div class="card-body" data-messageid="449">          
+  //                   <p class="fw-bold">What did you not like?</p>
+  //                   <button type="button" class="btn btn-sm feedback-button fw-bold mb-2" data-comment="Factually incorrect" data-messageid=${messageID} id="btnFactuallyIncorrect">
+  //                     Factually incorrect
+  //                   </button>
+  //                   <button type="button" class="btn btn-sm feedback-button fw-bold mb-2" data-comment="Generic response" data-messageid=${messageID} id="btnGenericResponse">
+  //                     Generic response
+  //                   </button>
+  //                   <button type="button" class="btn btn-sm feedback-button fw-bold mb-2" data-comment="Refused to answer" data-messageid=${messageID} id="btnRefusedToAnswer">
+  //                     Refused to answer
+  //                   </button>
+  //                   <button type="button" class="btn btn-sm feedback-other-button fw-bold mb-2" data-bs-toggle="modal" data-messageid=${messageID} data-bs-target="#modal-${messageID}" data-comment="Other" id="btnOther">
+  //                     Other
+  //                   </button>
+  //                   </div>
+  //                 </div>
+  //             </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //       <!-- Modal -->
+  //       <div class="modal fade" id="modal-${messageID}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  //         <div class="modal-dialog modal-dialog-centered">
+  //           <div class="modal-content">
+  //             <div class="modal-header">
+  //             <img class="waterdrop3">
+  //               <h1 class="modal-title fs-5" id="exampleModalLabel">
+  //                 &nbsp;
+  //                 Provide additional feedback
+  //               </h1>
+  //               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  //             </div>
+  //             <div class="modal-body">
                   
-                    <button type="button" class="btn btn-sm modal-feedback-button fw-bold mb-2" data-messageid=${messageID} data-comment="Factually incorrect" id="btnFactuallyIncorrect">
-                      Factually incorrect
-                    </button>
-                    <button type="button" class="btn btn-sm modal-feedback-button fw-bold mb-2" data-messageid=${messageID} data-comment="Generic response" id="btnGenericResponse">
-                      Generic response
-                    </button>
-                    <button type="button" class="btn btn-sm modal-feedback-button fw-bold mb-2" data-messageid=${messageID} data-comment="Refused to answer" id="btnRefusedToAnswer">
-                      Refused to answer
-                    </button>
-                    <button type="button" class="btn btn-sm modal-feedback-button fw-bold mb-2" data-messageid=${messageID} data-comment="Other" id="btnOther">
-                      Other
-                    </button>
-                    <textarea placeholder="Provide additional feedback" class="userComment form-control" data-feedback="" id ="userComment-${messageID}"></textarea>
+  //                   <button type="button" class="btn btn-sm modal-feedback-button fw-bold mb-2" data-messageid=${messageID} data-comment="Factually incorrect" id="btnFactuallyIncorrect">
+  //                     Factually incorrect
+  //                   </button>
+  //                   <button type="button" class="btn btn-sm modal-feedback-button fw-bold mb-2" data-messageid=${messageID} data-comment="Generic response" id="btnGenericResponse">
+  //                     Generic response
+  //                   </button>
+  //                   <button type="button" class="btn btn-sm modal-feedback-button fw-bold mb-2" data-messageid=${messageID} data-comment="Refused to answer" id="btnRefusedToAnswer">
+  //                     Refused to answer
+  //                   </button>
+  //                   <button type="button" class="btn btn-sm modal-feedback-button fw-bold mb-2" data-messageid=${messageID} data-comment="Other" id="btnOther">
+  //                     Other
+  //                   </button>
+  //                   <textarea placeholder="Provide additional feedback" class="userComment form-control" data-feedback="" id ="userComment-${messageID}"></textarea>
                   
-              </div>
-              <div id="footer-${messageID}" class="modal-footer" data-messageid=${messageID} >
-                <button class="comment btn btn-primary btn-new-chat" data-messageid=${messageID} data-user-comment-target=".userComment">Submit</button>                
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
-    chatHistory.appendChild(botMessage);
-    messageInterval(botResponse, messageID)
-  }
+  //             </div>
+  //             <div id="footer-${messageID}" class="modal-footer" data-messageid=${messageID} >
+  //               <button class="comment btn btn-primary btn-new-chat" data-messageid=${messageID} data-user-comment-target=".userComment">Submit</button>                
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     `;
+  //   chatHistory.appendChild(botMessage);
+  //   messageInterval(botResponse, messageID)
+  // }
   $(document).on('click', '.modal-feedback-button', function () {
     $(".modal-feedback-button").removeClass("selected");
     $(this).addClass("selected");
